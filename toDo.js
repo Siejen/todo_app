fbde95f769865c82ccd57f13e4ddf9af2d916900
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-07-14 22:55:26 -0700

    Committing changes to files

--- toDo.js
+++ toDo.js
@@ -3,6 +3,10 @@
 var form = document.getElementById('toDo_form');		//Line 3 gets a reference to the "toDo" form element
 var list = document.getElementById('toDo_list');
 
+var boxClick = function() {
+	this.style.backgroundColor = "red";
+}
+
 // single letter variables are bad!
 var addItem = function(item) {
 	//1. Create element i.e. create a new tag
@@ -11,12 +15,28 @@ var addItem = function(item) {
 	//4. Add the element from 1 to the list 
 
 	var newLi = document.createElement("li");				//New element created was the "list item" tag
-	var newItemNode = document.createTextNode(item.value); 	//A new text node was created which was the results of the user input (item.value)
+	var checkBoxDiv = document.createElement("div");		//New element created was the "div item" tag	
+	var itemTextDiv = document.createElement("div");		//New element created was the "div item" tag
+	var newItemText = document.createTextNode(item.value); 	//A new text node was created which was the results of the user input (item.value)
 
-	newLi.appendChild(newItemNode);
+	checkBoxDiv.className = "todoCheckBox";
+	checkBoxDiv.onclick = boxClick;
+
+
+    // 1. add the two divs to the list item tag (ie newLi)
+    // 2. add the text node to the itemTextDiv
+    // 3. add the new list item (newLi) to the list
+
+	itemTextDiv.appendChild(newItemText);
+	newLi.appendChild(checkBoxDiv);
+	newLi.appendChild(itemTextDiv);
 	list.appendChild(newLi);
+	
 	//console.log(item.value);
 	//list.innerHTML += "<li>" + item.value + "</li>"
+
+	//document.getElementById("toDo_form").reset();
+	form.reset();
 }
 
 form.onsubmit = function (event) {
